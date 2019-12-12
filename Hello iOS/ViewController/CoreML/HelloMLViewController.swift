58fd2939896dc7b79eccb065b37d4a69e9533069
tonynowater <tonynowater87@gmail.com>
2019-12-12 16:47:04 +0800

    Upgrade to Swift 5

---
+++ Hello iOS/ViewController/CoreML/HelloMLViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HelloMLViewController.swift
+//  Hello iOS
+//
+//  Created by Stan on 2019/8/26.
+//  Copyright © 2019 Tony. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+import AVFoundation
+
+//Vision
+//
+//文字偵測
+//目標追蹤
+//圖像配置
+//矩形偵測
+//image -> model -> object
+class HelloMLViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
